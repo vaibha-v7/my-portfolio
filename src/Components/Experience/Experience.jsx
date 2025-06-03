59b3a0d449de0be892efb85612d769951df73d1c
vaibha_v7 <vaibhav00070@gmail.com>
2025-06-03 16:23:12 +0530

    experience done

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -10,7 +10,7 @@ const Experience = () => {
     <div id="Experience" className="p-10 md:p-24">
       <h1 className="text-2xl md:text-4xl text-white font-bold">Experience</h1>
       <div className="flex flex-wrap items-center justify-around">
-        <div className="flex flex-wrap md:w-2/5 gap-8 md:p-12 py-10">
+        <div className="flex justify-center flex-wrap md:w-2/5 gap-8 md:p-12 py-10 ">
           <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
             <FaHtml5 color="#E34F26" size={50} />
           </span>
