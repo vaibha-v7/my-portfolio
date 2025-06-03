c3cb99a90833debe5c213197f6039e06b5c088c3
vaibha_v7 <vaibhav00070@gmail.com>
2025-06-03 16:08:01 +0530

    project pannel done

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 // import bannerImg from "../../assets/photo-C8q0KQHG.webp";
-const ProjectCard = ({ title, main }) => {
+const ProjectCard = ({ title, main , img ,demo ,sourcecode }) => {
   return (
     <div className="p-3 md:p-6 flex flex-col w-80 bg-[#0c0e19] shadow-xl shadow-slate-900 rounded-2xl">
-      
-      <h3 className="px-4 text-xl md:text-2xl font-bold leading-normal">
+      <img className=" w-[100%] rounded-xl" src={img} alt="hlo" />
+      <h3 className="mt-8 px-4 text-xl md:text-2xl font-bold leading-normal">
         {title}
       </h3>
-      <p className="px-4 text-sm md:text-md leading-tight py-2">{main}</p>
+      <p className="px-4 text-base md:text-md  py-2">{main}</p>
+
+
+
+
       <div className="mt-2 p-2 md:p-4 flex gap-2 md:gap-4">
+
+        <a target="_blank" rel="noopener noreferrer" href={demo}>
         <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
           Demo
         </button>
+        </a>
+
+
+        <a target="_blank" rel="noopener noreferrer" href={sourcecode}>
         <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
           Source Code
         </button>
+        </a>
+
       </div>
+
+
+
+
     </div>
   );
 };
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -7,16 +7,25 @@ const Projects = () => {
       <h1 className="text-2xl md:text-4xl text-white font-bold">Projects</h1>
       <div className="py-12 px-8 flex flex-wrap gap-5 items-center justify-center">
         <ProjectCard
-          title="Blogging Website"
-          main="this is a bloggin website created in next js and used some component library used some component library"
+          img="https://arjancodes.com/_astro/how-to-build-a-simple-discord-bot-leading.1JGv7MUV.jpeg"
+          title="Discord Bot"
+          main="A feature-rich Discord music bot that supports multiple music platforms and languages"
+          demo="https://discord.com/oauth2/authorize?client_id=1342201785317851249"
+          sourcecode="https://github.com/vaibha-v7/courage_bot"
         />
         <ProjectCard
-          title="Youtue Clone"
-          main="this is a bloggin website created in next js and used some component library used some component library"
+        img="https://mediumrare.imgix.net/30688668d7d2d48d472edd0f1e2bca0758e7ec51cbab8c04d8b7f157848640e0?q=85"
+          title="Dice Game"
+          main="its a basic dice game created using react was a great starting project for learning basics of react"
+          demo="https://gilded-smakager-4108a0.netlify.app/"
+          sourcecode="https://github.com/vaibha-v7/Dice_Game"
         />
         <ProjectCard
-          title="Netflix Clone"
-          main="nt library used some component library"
+          img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAYC5ELcypMSxCl_0Z72QTMnhJz7rwMcol9Q&s"
+          title="Simon Game"
+          main="A fun memory game where you have to repeat the color sequence! Built with HTML, CSS, and JavaScript."
+          demo="https://vaibha-v7.github.io/simon-says-game/"
+          sourcecode="https://github.com/vaibha-v7/simon-says-game"
         />
       </div>
     </div>
