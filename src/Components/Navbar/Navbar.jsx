d72be817fb4efee346f7b10b037231618e1b0c3d
vaibha_v7 <vaibhav00070@gmail.com>
2025-06-03 14:54:50 +0530

    navbar fixd

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -46,6 +46,10 @@ const Navbar = () => {
         <RiCloseLine
           size={30}
           className="md:hidden absolute right-10 top-6 transition-all duration-300"
+          onClick={() => {
+            openMenu(!menu);
+            setShowmenu(!showMenu);
+          }}
         />
       )}
     </nav>
